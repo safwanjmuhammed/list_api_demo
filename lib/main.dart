7857b30c905be06c6bfecc8b590bcd8dd510d0f1
safwanjmuhammed <safwanjmuhammed@gmail.com>
2023-11-18 18:06:14 +0530

    hg

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 142, 110, 196)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 142, 110, 196)),
         useMaterial3: false,
       ),
       home: const HomePage(),
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -28,33 +28,47 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: listData.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : Center(
-              child: GridView.builder(
-              // shrinkWrap: true,
-              padding: EdgeInsets.all(5),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2),
-              itemCount: listData.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        image: DecorationImage(
-                            image: NetworkImage(listData[index].url))),
-                    height: 40,
-                    width: 20,
-                    child: Stack(
-                      children: [Text(listData[index].title)],
+    return SafeArea(
+      child: Scaffold(
+        body: listData.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : Center(
+                child: GridView.builder(
+                // shrinkWrap: true,
+                padding: EdgeInsets.all(5),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemCount: listData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(20)),
+                          image: DecorationImage(
+                              image: NetworkImage(listData[index].url))),
+                      height: 40,
+                      width: 20,
+                      child: Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(listData[index].id.toString()),
+                                Text(listData[index].title.toString()),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              },
-            )),
+                  );
+                },
+              )),
+      ),
     );
   }
 }
